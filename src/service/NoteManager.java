de52f66eb2a9459c7e675077f7be63dd1630f6fd
Raitis-Zeile <zeile.raitis@jak.lv>
2026-04-13 08:54:04 +0000

    darbs

--- src/service/NoteManager.java
+++ src/service/NoteManager.java
@@ -3,4 +3,54 @@ import model.Note;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public 
+public class NoteManager{
+    private ArrayList<Note> notes = new ArrayList<>();
+
+    public void addNote(Note note){
+        notes.add(note);
+    }
+
+    public void printAllNotes(){
+        if (notes.isEmty()) {
+            System.out.println("Nav nevienas piezimes");
+            return;
+        }
+
+        for (Note n : notes){
+            n.print();
+        }
+    }
+
+    public void search(String keyword){
+        boolean found = false;
+
+        for (Note n: notes){
+            if(n.getTitle().toLowerCase().contains(keyword.toLowerCase())||
+
+            n.getContent().toLowerCase().contain(keyword.toLowerCase())){
+
+                 n.print();
+                 found = true;
+            }
+        }
+
+        if(!found){
+            System.out.println("Nekas netika atrasts.");
+        }
+    }
+
+    public void groupByCategory(){
+        HashMap<String, ArrayList<Note>> map = new HashMap<>();
+
+        for(Note n : notes){
+            map.putIfAbsent(n.getCategory(), new ArrayList<>());
+            map.get(n.getCategory()).add(n);
+        }
+        fro(String category : map.key()){
+            System.out.println("\nKategorija:" + category);
+            for(Note n : map.get(category)){
+                n.print();
+            }
+        }
+    }
+}
